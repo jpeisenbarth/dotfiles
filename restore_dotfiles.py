cbd0116dfafcadc1edb1edb3099b62aebac420b4
Jean-Philippe Eisenbarth <jp@eisenbarth.io>
2017-09-08 16:35:46 +0200

    [fix] gitconfig file

--- restore_dotfiles.py
+++ restore_dotfiles.py
@@ -5,7 +5,7 @@ import shutil
 import errno
 
 
-ignore_files = ["README.md", ".git", ".gitignore", "restore_dotfiles.py", ".config"]
+ignore_files = ["README.md", ".git", ".gitignore", "restore_dotfiles.py", ".config", ".gitconfig"]
 files = [f for f in os.listdir() if f not in ignore_files]
 files += [f for f in os.listdir(".config")]
 home_directory = os.path.expanduser("~")
@@ -35,6 +35,10 @@ for f in files:
             raise e
 
 os.system("git clone https://github.com/chriskempson/base16-shell.git .config/base16-shell")
+try:
+    shutil.copy2(".gitconfig", os.path.join(home_directory, ".gitconfig"))
+    print("Do not forget to update the email address in the .gitconfig file")
+except shutil.SameFileError:
+    pass
 
 print("Finished")
-print("Do not forget to update the email address in the .gitconfig file")
