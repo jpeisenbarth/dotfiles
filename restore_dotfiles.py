7427a7422e0c0d829cbde07ec50672cfc1304cef
Jean-Philippe Eisenbarth <jp@eisenbarth.io>
2017-09-08 16:26:03 +0200

    [update] dotfiles

--- restore_dotfiles.py
+++ restore_dotfiles.py
@@ -1,10 +1,13 @@
 #! /usr/bin/env python3
 
 import os
+import shutil
+import errno
 
 
 ignore_files = ["README.md", ".git", ".gitignore", "restore_dotfiles.py", ".config"]
 files = [f for f in os.listdir() if f not in ignore_files]
+files += [f for f in os.listdir(".config")]
 home_directory = os.path.expanduser("~")
 
 print("Restoring the dotfiles ...")
@@ -13,12 +16,25 @@ if not os.path.isdir(os.path.join(home_directory, ".config/autostart")):
     os.makedirs(os.path.join(home_directory, ".config/autostart"))
 
 for f in files:
-    abspath_f = os.path.abspath(f)
-    os.symlink(abspath_f, os.path.join(home_directory, f), target_is_directory=True)
+    abspath_source = os.path.abspath(f)
+    print(abspath_source)
+    abspath_dest = os.path.join(home_directory, f)
+    try:
+        os.symlink(abspath_source, abspath_dest, target_is_directory=True)
+    except OSError as e:
+        if e.errno == errno.EEXIST:
+            if os.path.isdir(abspath_dest):
+                if os.path.islink(abspath_dest):
+                    os.unlink(abspath_dest)
+                else:
+                    shutil.rmtree(abspath_dest)
+            else:
+                os.remove(abspath_dest)
+            os.symlink(abspath_source, abspath_dest, target_is_directory=True)
+        else:
+            raise e
 
-os.symlink(".config/base16-shell", os.path.join(home_directory, ".config/base16-shell"), target_is_directory=True)
-os.symlink(".config/terminator", os.path.join(home_directory, ".config/terminator"), target_is_directory=True)
-os.symlink(".config/redshift.conf", os.path.join(home_directory, ".config/redshift.conf"))
+os.system("git clone https://github.com/chriskempson/base16-shell.git .config/base16-shell")
 
 print("Finished")
 print("Do not forget to update the email address in the .gitconfig file")
