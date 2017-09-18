056ef25919a7d9bc40906fad9aac9c737d76f0ca
Jean-Philippe Eisenbarth <jp@eisenbarth.io>
2017-09-18 11:07:31 +0200

    [update] Dotfiles

--- lock.sh
+++ lock.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-xdotool keyup super keyup l key XF86AudioPlay
 xdg-screensaver lock
 exit 0
--- lock_suspend.sh
+++ lock_suspend.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-xdotool keyup shift keyup super keyup l key XF86AudioPlay
 xdg-screensaver lock; systemctl -i suspend
 exit 0
